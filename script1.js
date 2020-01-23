777ab136b3f8bf8ff3f254bf0b39ab2e22428300
Dr.UvX <35187794+u99d88@users.noreply.github.com>
2020-01-23 15:04:05 +0200

    update js

--- script1.js
+++ script1.js
@@ -1,17 +1,8 @@
-var plan1 = document.getElementById("plan1");
-var plan2 = document.getElementById("plan2");
-var plan4 = document.getElementById("plan4");
-var plan_speak = document.getElementById("plan_speak");
-var go_btn =  document.getElementById("input_submit");
-var start_form = document.getElementById("form_wrapper");
-var main_canvas = document.getElementById("main_canvas");
-
-//default
-var chosen_plan = plan1;
+var chosen_plan = 1;
 plan1.style.border = '3px solid #002366';
 
-plan1.addEventListener("click", function () {
-    chosen_plan = plan1;
+$("#plan1").click(function () {
+    chosen_plan = 1;
 
     plan1.style.border = '3px solid #002366';
     plan2.style.border = 'none';
@@ -19,8 +10,8 @@ plan1.addEventListener("click", function () {
     plan_speak.style.border = 'none';
 });
 
-plan2.addEventListener("click", function () {
-    chosen_plan = plan2;
+$("#plan2").click(function () {
+    chosen_plan = 2;
 
     plan1.style.border = 'none';
     plan2.style.border = '3px solid #002366';
@@ -28,8 +19,8 @@ plan2.addEventListener("click", function () {
     plan_speak.style.border = 'none';
 });
 
-plan4.addEventListener("click", function () {
-    chosen_plan = plan4;
+$("#plan4").click(function () {
+    chosen_plan = 4;
 
     plan1.style.border = 'none';
     plan2.style.border = 'none';
@@ -37,8 +28,8 @@ plan4.addEventListener("click", function () {
     plan_speak.style.border = 'none';
 });
 
-plan_speak.addEventListener("click", function () {
-    chosen_plan = plan_speak;
+$("#plan_speak").click(function () {
+    chosen_plan = 0;
 
     plan1.style.border = 'none';
     plan2.style.border = 'none';
@@ -46,15 +37,16 @@ plan_speak.addEventListener("click", function () {
     plan_speak.style.border = '3px solid #002366';
 });
 
-go_btn.addEventListener("click", function () {
-    start_form.style.display = 'none';
-    main_canvas.style.display = 'block';
+$("#input_submit").click(function () {
+    $("#form_wrapper").hide();
+    $("#main_canvas").show();
     start_amp();
 });
 
 var start_amp = function () {
     'use strict';
     var boardArray = new Array();
+    var totalSamples = 0;
 
     var soundAllowed = function (stream) {
         window.persistAudioStream = stream;
@@ -65,7 +57,7 @@ var start_amp = function () {
         analyser.fftSize = 2048;
 
         var frequencyArray = new Uint8Array(analyser.frequencyBinCount);
-
+        
         var doDraw = function () {
             requestAnimationFrame(doDraw);
             analyser.getByteTimeDomainData(frequencyArray);
@@ -77,7 +69,13 @@ var start_amp = function () {
                 }
             }
 
-            boardArray.push(max);
+            totalSamples += 1;
+            if (totalSamples == 60)
+            {
+                totalSamples = 0;
+            }
+
+            boardArray.push([max, totalSamples == 0]);
             if (boardArray.length >= document.body.clientWidth - 100) {
                 boardArray.shift();
             }
@@ -88,18 +86,13 @@ var start_amp = function () {
     }
 
     var soundNotAllowed = function (error) {
-        alert('You must allow microphone access');
+        alert('Please check your microphone connectivity and allow this site to access it');
         console.log(error);
     }
 
     navigator.getUserMedia({ audio: true }, soundAllowed, soundNotAllowed);
 }
 
-/**
- * Filters the AudioBuffer retrieved from an external source
- * @param {AudioBuffer} audioBuffer the AudioBuffer from drawAudio()
- * @returns {Array} an array of floating point numbers
- */
 const filterData = audioBuffer => {
     const rawData = audioBuffer.getChannelData(0); // We only need to work with one channel of data
     const samples = 70; // Number of samples we want to have in our final data set
@@ -116,11 +109,6 @@ const filterData = audioBuffer => {
     return filteredData;
 };
 
-/**
- * Normalizes the audio data to make a cleaner illustration 
- * @param {Array} filteredData the data from filterData()
- * @returns {Array} an normalized array of floating point numbers
- */
 const normalizeData = filteredData => {
     const multiplier = Math.pow(Math.max(...filteredData), -1);
     return filteredData.map(n => n * multiplier);
@@ -130,18 +118,12 @@ const dataToCanvas = filteredData => {
     const newData = [];
 
     for (let i = 0; i < filteredData.length; i++) {
-        newData.push(document.body.clientHeight - (filteredData[i] / 255) * document.body.clientHeight);
+        newData.push([document.body.clientHeight - (filteredData[i][0] / 255) * document.body.clientHeight, filteredData[i][1]]);
     }
 
     return newData;
 }
 
-
-/**
- * Draws the audio file into a canvas element.
- * @param {Array} normalizedData The filtered array returned from filterData()
- * @returns {Array} a normalized array of data
- */
 const draw = normalizedData => {
     // set up the canvas
     const canvas = document.querySelector("canvas");
@@ -156,40 +138,25 @@ const draw = normalizedData => {
 
     newData = dataToCanvas(normalizedData);
 
+    ctx.beginPath();
     ctx.lineWidth = 1;
     ctx.strokeStyle = "#65b042";
-    ctx.beginPath();
-    //ctx.moveTo(0,canvas.height);
-    //ctx.lineTo(canvas.width,canvas.height);
     ctx.moveTo(0, newData[0]);
     for (let i = 0; i < newData.length; i++) {
-        ctx.lineTo(i, newData[i]);
-
-        if (i%100 == 0) {
-            ctx.moveTo(i, 0);
-            ctx.lineTo(i,400);
-            ctx.moveTo(i, newData[i]);
-        }
+        ctx.lineTo(i, newData[i][0]);
     }
     ctx.stroke();
-};
 
-/**
- * A utility function for drawing our line segments
- * @param {AudioContext} ctx the audio context 
- * @param {number} x  the x coordinate of the beginning of the line segment
- * @param {number} height the desired height of the line segment
- * @param {number} width the desired width of the line segment
- * @param {boolean} isEven whether or not the segmented is even-numbered
- */
-const drawLineSegment = (ctx, x, height, width, isEven) => {
-    ctx.lineWidth = 1; // how thick the line is
-    ctx.strokeStyle = "#444"; // what color our line is
     ctx.beginPath();
-    height = isEven ? height : -height;
-    ctx.moveTo(x, 0);
-    ctx.lineTo(x, height);
-    ctx.arc(x + width / 2, height, width / 2, Math.PI, 0, isEven);
-    ctx.lineTo(x + width, 0);
+    ctx.lineWidth = 0.5;
+    ctx.strokeStyle = "#ffffff";
+    for (let i = 0; i < newData.length; i++) {
+        if (newData[i][1])
+        {
+            ctx.moveTo(i, 0);
+            ctx.lineTo(i, document.body.clientHeight);
+
+        }
+    }
     ctx.stroke();
 };
